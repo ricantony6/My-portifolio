1e32d15bdcd754bff787b68c873411347264c93c
Ricardo <ricantony66@gmail.com>
2025-08-23 08:15:35 -0300

    Atualizações e melhorias no portfólio

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-
+// Seleciona o botão de alternância de tema
 let themeToggler = document.querySelector('#theme-toggler');
 
-themeToggler.onclick = () =>{
+// Adiciona um evento de clique ao botão
+themeToggler.onclick = () => {
+    // Alterna a classe 'fa-sun' no ícone do botão
     themeToggler.classList.toggle('fa-sun');
-    if(themeToggler.classList.contains('fa-sun')){
-        document.body.classList.add('active');
-    }else{
-        document.body.classList.remove('active');
+
+    // Se o botão tem a classe 'fa-sun', aplica o tema escuro no body
+    if (themeToggler.classList.contains('fa-sun')) {
+        document.body.classList.add('active'); // Tema escuro ativado
+    } else {
+        document.body.classList.remove('active'); // Tema claro ativado
     }
-}
+};
